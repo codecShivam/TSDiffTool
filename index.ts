aff2d0e999e364c40904c06a1bd45429cd0c6e03
codecShivam <codecshivam@gmail.com>
2024-01-03 20:33:00 +0530

    defined sub-command class

--- index.ts
+++ index.ts
@@ -1,4 +1,20 @@
-import * as fs from 'fs';
+import * as fs from "fs";
 
-const words = fs.readFileSync('./file1.txt', 'utf-8');
-console.log(words);
+function readEntireFile(filePath: string): string {
+  console.log(`Reading file: ${filePath}`);
+  const content = fs.readFileSync(filePath, "utf8");
+  console.log(`File content: ${content}`);
+  return content;
+}
+
+class Subcommand {
+  name: string;
+  signature: string;
+  description: string;
+
+  constructor(name: string, signature: string, description: string) {
+    this.name = name;
+    this.signature = signature;
+    this.description = description;
+  }
+}
